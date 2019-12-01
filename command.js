d1f4759b6157998deaf5176567986224a7fce2da
xiaohuli <waterdoor@foxmail.com>
2019-12-01 10:36:10 +0800

    fix: 修改命令行

--- command.js
+++ command.js
@@ -7,15 +7,17 @@ const stdout = process.stdout;
 const gWord = chalk.green;
 const rWord = chalk.red;
 
-const contents = ['1、show files tree', '2、say hello to the world', '3、Release Edition', '4、Export Out Files', '5、Exit'];
+const contents = ['1、show files tree', '2、say hello to the world', '3、Release Edition', '4、Export Out Files', 
+    '5、build project',
+    '6、Exit'];
 console.log(chalk.yellow('Choose what you want to do, enter number please:'));
 contents.forEach(item => {
     console.log(gWord(item));
 })
 stdin.resume();
 stdin.on('data', async function(data) {
-    //  输入获得的是buffer 对象，不能直接使用，否则会有问题
-    const enterString = data.toString().slice(0, -2);
+    //  输入获得的是buffer 对象，不能直接使用，否则会有问题， 貌似window下和linux下的格式不对应，linux下应该为slice(0,-1)
+    const enterString = data.toString().slice(0,-1);
     if (!/[0-9]+$/.test(enterString)) {
         console.log(rWord('Input Words is not Number!'));
         return ;
@@ -25,7 +27,8 @@ stdin.on('data', async function(data) {
         2: () => { console.log(rWord('hello World!')) },
         3: () => getWrapperPromise('bash ./script/relese.sh'),
         4: () => getWrapperPromise('bash ./script/export.sh'),
-        5: () => {}
+        5: () => getWrapperPromise('babel-node ./bundle'),
+        6: () => {}
     }
     const targetFunc = funcMap[enterString];
     if (targetFunc) {
